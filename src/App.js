028d08f28fc68b0aa5bab912b0a5139a1c4e73ce
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-05-16 17:02:15 -0300

    All questions added to the code

--- src/App.js
+++ src/App.js
@@ -25,7 +25,43 @@ export default function App (){
         cardStep: 0,
         option: false,
         color:false
+    },
+    {
+        question: "Podemos colocar __ dentro do JSX",
+        answer: "expressões",
+        cardStep: 0,
+        option: false,
+        color:false
+    },
+    {
+        question: "O ReactDOM nos ajuda __",
+        answer: "interagindo com a DOM para colocar componentes React na mesma",
+        cardStep: 0,
+        option: false,
+        color:false
+    },
+    {
+        question: "Usamos o npm para __",
+        answer: "gerenciar os pacotes necessários e suas dependências",
+        cardStep: 0,
+        option: false,
+        color:false
+    },
+    {
+        question: "Usamos props para __",
+        answer: "passar diferentes informações para componentes",
+        cardStep: 0,
+        option: false,
+        color:false
+    },
+    {
+        question: "Usamos estado (state) para __",
+        answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+        cardStep: 0,
+        option: false,
+        color:false
     }];
+
     const [init, setInit] = React.useState(false);
     const [cards, setCards] = React.useState(objCards);
 
