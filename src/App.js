a148e2859c78e191612fa31c365d3c8fcc61e169
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-05-16 21:06:59 -0300

    randomize cards

--- src/App.js
+++ src/App.js
@@ -4,66 +4,72 @@ import Zap from "./pages/Zap"
 import "./Assets/css/reset.css"
 import "./Assets/css/style.css"
 
+const objCards = [{
+    question: "O que é JSX?",
+    answer: "Uma extensão de linguagem do JavaScript",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question:"O React é __",
+    answer: "uma biblioteca JavaScript para construção de interfaces",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question: "Componentes devem iniciar com __",
+    answer: "letra maiúscula",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question: "Podemos colocar __ dentro do JSX",
+    answer: "expressões",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question: "O ReactDOM nos ajuda __",
+    answer: "interagindo com a DOM para colocar componentes React na mesma",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question: "Usamos o npm para __",
+    answer: "gerenciar os pacotes necessários e suas dependências",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question: "Usamos props para __",
+    answer: "passar diferentes informações para componentes",
+    cardStep: 0,
+    option: false,
+    color:false
+},
+{
+    question: "Usamos estado (state) para __",
+    answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+    cardStep: 0,
+    option: false,
+    color:false
+}];
+
+let data = objCards.map((card) => card);
+
+data = data.sort((a, b) => Math.random() - 0.5);
+
 export default function App (){
-    const objCards = [{
-        question: "O que é JSX?",
-        answer: "Uma extensão de linguagem do JavaScript",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question:"O React é __",
-        answer: "uma biblioteca JavaScript para construção de interfaces",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question: "Componentes devem iniciar com __",
-        answer: "letra maiúscula",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question: "Podemos colocar __ dentro do JSX",
-        answer: "expressões",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question: "O ReactDOM nos ajuda __",
-        answer: "interagindo com a DOM para colocar componentes React na mesma",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question: "Usamos o npm para __",
-        answer: "gerenciar os pacotes necessários e suas dependências",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question: "Usamos props para __",
-        answer: "passar diferentes informações para componentes",
-        cardStep: 0,
-        option: false,
-        color:false
-    },
-    {
-        question: "Usamos estado (state) para __",
-        answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
-        cardStep: 0,
-        option: false,
-        color:false
-    }];
+    
 
     const [init, setInit] = React.useState(false);
-    const [cards, setCards] = React.useState(objCards);
+    const [cards, setCards] = React.useState(data);
 
     return(
         <>
